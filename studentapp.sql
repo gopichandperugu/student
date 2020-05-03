ae4bd0a6a46a34bc86d5342c105138481919a01a
devops090 <51469018+devops090@users.noreply.github.com>
2020-05-04 00:23:08 +0530

    Update studentapp.sql
    hey

--- studentapp.sql
+++ studentapp.sql
@@ -2,7 +2,7 @@ create database studentapp;
 use studentapp;
 CREATE TABLE Students(student_id INT NOT NULL AUTO_INCREMENT,
 	student_name VARCHAR(100) NOT NULL,
-    student_addr VARCHAR(100) NOT NULL,
+        student_addr VARCHAR(100) NOT NULL,
 	student_age VARCHAR(3) NOT NULL,
 	student_qual VARCHAR(20) NOT NULL,
 	student_percent VARCHAR(10) NOT NULL,
